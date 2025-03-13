00c7b9a4029787c53c6544111116f5de6b2fda31
matuza1977 <matuzagames@gmail.com>
2025-03-13 20:40:08 -0300

    Primeiro Commit

---
+++ static/app.js
@@ -0,0 +1,32 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let nomes = [];
+
+function adicionarAmigo() {
+    let inputNome = document.getElementById("amigo").value.trim();
+    let listaAmigos = document.getElementById("listaAmigos");
+    
+    if (inputNome === "") {
+        alert("Por favor, insira um nome válido.");
+    } else {
+        nomes.push(inputNome);
+        let novoItem = document.createElement("li");
+        novoItem.textContent = inputNome;
+        listaAmigos.appendChild(novoItem);
+        document.getElementById("amigo").value = ""; // Limpar campo
+    }
+}
+
+function sortearAmigo() {
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = ""; // Limpa a lista anterior
+    
+    if (nomes.length === 0) {
+        alert("Nenhum nome disponível para o sorteio.");
+    } else {
+        let sorteado = nomes[Math.floor(Math.random() * nomes.length)];
+        let itemResultado = document.createElement("li");
+        itemResultado.textContent = `O amigo secreto sorteado foi: ${sorteado}`;
+        resultado.appendChild(itemResultado);
+    }
+}
